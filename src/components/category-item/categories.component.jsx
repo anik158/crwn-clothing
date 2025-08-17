41b5802e763e84b2d27285f90c437bd3aeaab695
Ahsan <ahsanan@outlook.com>
2025-08-17 23:20:21 +0600

    category card complete

---
+++ src/components/category-item/categories.component.jsx
@@ -0,0 +1,26 @@
+
+import './categories.style.scss';
+
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+
+  return (
+    <div
+      className="category-item"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    >
+
+      <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop Now</p>
+      </div>
+      
+    </div>
+  );
+};
+
+ 
+
+export default CategoryItem;
